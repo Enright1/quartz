95585cbda6cbdf6bcae6b202ce016e671e7558a5
Enright1 <bmcdavies888@gmail.com>
2025-01-31 02:29:58 +1300

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -25,12 +25,8 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
   ],
 }
 
@@ -42,7 +38,6 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [],
 }
