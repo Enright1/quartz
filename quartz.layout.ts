4ba1b8cabd20eb2292a7c87a045a042d653b6a3b
Enright1 <bmcdavies888@gmail.com>
2025-01-31 00:03:25 +1300

    Revert "Update quartz.layout.ts to have TOC where exporer is on desktop"
    This reverts commit d3b517e367f289a7f7b928a6ae630a7a64757f14.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,7 +27,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.TableOfContents()),
+    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
     Component.Graph(),
